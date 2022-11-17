d56f028fca64c877a07f97cfb6eb29995707e4b9
ssagar9495 <117368325+ssagar9495@users.noreply.github.com>
2022-11-17 12:34:07 +0530

    Social Icon Row

---
+++ socialIconRow/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const SocialIconRow = () => {
+  return (
+    <>
+      <div className={styles.socialIconRow}>
+        <div className={styles.socialIconConatiner}>
+          <img src={/*image Url */} className={styles.socialIcon} />
+        </div>
+
+        <div className={styles.socialIconConatiner}>
+          <img src={/*image Url */} className={styles.socialIcon} />
+        </div>
+
+        <div className={styles.socialIconConatiner}>
+          <img src={/*image Url */} className={styles.socialIcon} />
+        </div>
+
+        <div className={styles.socialIconConatiner}>
+          <img src={/*image Url */} className={styles.socialIcon} />
+        </div>
+
+        <div className={styles.socialIconConatiner}>
+          <img src={/*image Url */} className={styles.socialIcon} />
+        </div>
+
+        <div className={styles.socialIconConatiner}>
+          <img src={/*image Url */} className={styles.socialIcon} />
+        </div>
+      </div>
+    </>
+  );
+};
+export default SocialIconRow;
