76b3b67badd813117a257c6a7551f4d161506f9b
ssagar9495 <117368325+ssagar9495@users.noreply.github.com>
2022-11-17 15:25:27 +0530

    Stock Heading

---
+++ headingText/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HeadingText = () => {
+  return (
+    <>
+      <div className={styles.HeadingText}>Hi There,</div>
+      <div className={styles.HeadingText}>
+        I'm Sagar, <span>Rana</span>
+      </div>
+    </>
+  );
+};
+export default HeadingText;
